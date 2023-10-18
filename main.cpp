82c2850b26fdfb7eac2f20c69e3d5eaa8d1452a9
Mikita <nik.podterob@gmail.com>
2023-10-18 22:17:18 +0300

    Add bot handling messages

---
+++ main.cpp
@@ -0,0 +1,61 @@
+// 6616053114:AAHLzPA5NKlyYOzkrq2UPz0dUXSYVcH-OX0
+// -668390686
+#include <stdio.h>
+#include <tgbot/tgbot.h>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+int main() {
+    srand(time(NULL));  // Seed the random number generator
+    
+    std::vector<int> allowedChats = {-668390686};
+    const int ResponseRate = 3;
+    
+    TgBot::Bot bot("6616053114:AAEON-uUjywJkW7RGQ77Lg_78tbS1oNGbV4");
+    std::vector<std::string> responses = {"ok"};
+    int messageCount = 0;
+    time_t lastUpdateTime = time(NULL);
+    
+bot.getEvents().onAnyMessage([&bot, &messageCount, &responses, &allowedChats, &lastUpdateTime](TgBot::Message::Ptr message) {
+    try {
+        if (message->date < lastUpdateTime) { return; } // accept only new mwssages
+                                
+        messageCount++;
+        auto it = std::find(allowedChats.begin(), allowedChats.end(), message->chat->id);
+        if(it != allowedChats.end()){
+            
+            TgBot::ChatMember::Ptr member = bot.getApi().getChatMember(message->chat->id, bot.getApi().getMe()->id);
+            if (member->status == "kicked" || member->status == "left") {
+                allowedChats.erase(it);
+                return; // do not handdle if bot not in a chat
+            }
+
+            if (!(messageCount % ResponseRate)) { // reply to every n'th message
+                int randomIndex = rand() % responses.size();
+                bot.getApi().sendMessage(message->chat->id, responses[randomIndex], true, message->messageId);
+            }
+        }else{
+            bot.getApi().sendMessage(message->chat->id, "Извините, этот бот только для АВТОМАТИЗАЦИИ");
+            bot.getApi().leaveChat(message->chat->id);
+        }
+                            
+        lastUpdateTime = time(NULL);  // Update the last update time
+    } catch (TgBot::TgException& e) {
+        printf("error: %s\n", e.what());
+    }
+});
+
+    try {
+        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
+        TgBot::TgLongPoll longPoll(bot);
+        
+        while (true) {
+            printf("Long poll started\n");
+            longPoll.start();
+        }
+    } catch (TgBot::TgException& e) {
+        printf("error: %s\n", e.what());
+    }
+    return 0;
+}
